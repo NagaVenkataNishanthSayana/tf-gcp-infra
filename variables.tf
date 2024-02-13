9bf5104b3e0d1481f9d705216a9c17ea28577851
Naga Venkata Nishanth Sayana <sayana.n@northeastern.edu>
2024-02-13 15:47:49 -0500

    terraform-vpc

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "project" {
+  description = "GCP project name"
+  default     = "cloud-networking-project"
+}
+
+variable "region" {
+  description = "GCP region"
+  default     = "us-east1"
+}
+
+variable "network_name" {
+  description = "Name of the Google Compute Engine network"
+  default     = "cloudvpc"
+}
+
+variable "webapp_subnet_cidr" {
+  description = "CIDR range for the webapp subnet"
+  default     = "10.0.1.0/24"
+}
+
+variable "db_subnet_cidr" {
+  description = "CIDR range for the db subnet"
+  default     = "10.0.2.0/24"
+}
+
+variable "webapp_route_dest_range" {
+  description = "Destination IP range for the webapp route"
+  default     = "0.0.0.0/0"
+}
+
+variable "webapp_route_priority" {
+  description = "Priority for the webapp route"
+  default     = 1000
+}
+
+variable "webapp_route_tags" {
+  description = "Tags for the webapp route"
+  default     = ["webapp"]
+}
+
+variable "next_hop_gateway" {
+  description = "Next hop gateway for the route"
+  default     = "default-internet-gateway"
+}
---
+++ vpc.tf
@@ -0,0 +1,33 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+resource "google_compute_network" "cloud_vpc" {
+  name                    = var.network_name
+  auto_create_subnetworks = false
+  routing_mode            = "REGIONAL"
+}
+
+resource "google_compute_subnetwork" "webapp_subnet" {
+  name          = "webapp"
+  network       = google_compute_network.cloud_vpc.self_link
+  ip_cidr_range = var.webapp_subnet_cidr
+  region        = var.region
+}
+
+resource "google_compute_subnetwork" "db_subnet" {
+  name          = "db"
+  network       = google_compute_network.cloud_vpc.self_link
+  ip_cidr_range = var.db_subnet_cidr
+  region        = var.region
+}
+
+resource "google_compute_route" "webapp_route" {
+  name             = "webapp-route"
+  network          = google_compute_network.cloud_vpc.self_link
+  dest_range       = var.webapp_route_dest_range
+  priority         = var.webapp_route_priority
+  next_hop_gateway = var.next_hop_gateway
+  tags             = var.webapp_route_tags
+}
